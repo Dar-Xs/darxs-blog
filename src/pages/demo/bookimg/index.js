a84be1ff9bc1af42c30331c9328ab0dd983ba50b
Dr.Xiong <1393744283@qq.com>
2024-02-14 03:20:04 +0800

    feat: static book img scale effect

--- src/pages/demo/bookimg/index.js
+++ src/pages/demo/bookimg/index.js
@@ -1,39 +1,59 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import styles from "./index.module.scss";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
-export default function Portfolio() {
+export default function BookImg() {
+  const focusState = useRef(0);
+  const isFirstRender = useRef(true);
+  const [scale, setScale] = useState(1);
   const [xOffsetAngle, setXOffsetAngle] = useState(0);
   const [yOffsetAngle, setYOffsetAngle] = useState(0);
   const [transitionTime, setTransitionTime] = useState(0.5);
   const bookContainer = useRef(null);
-  const [resetTimeout, setResetTimeout] = useState(null);
   const moveTo = (e) => {
     if (!bookContainer.current) return;
-    setTransitionTime(Math.max(transitionTime * 0.9, 0.1));
     const rect = bookContainer.current.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
-    setXOffsetAngle(-((y - rect.height / 2) / rect.height) * 20);
-    setYOffsetAngle(((x - rect.width / 2) / rect.width) * 30);
-    clearTimeout(resetTimeout);
-    setResetTimeout(
-      setTimeout(() => {
-        focus();
-      }, 1000)
-    );
+    setScale(1.08);
+    if (focusState.current === 1) {
+      setXOffsetAngle(-((y - rect.height / 2) / rect.height) * 20);
+      setYOffsetAngle(((x - rect.width / 2) / rect.width) * 20);
+    } else if (focusState.current === 2) {
+      setTransitionTime(Math.max(transitionTime * 0.9, 0.1));
+      setXOffsetAngle(-((y - rect.height / 2) / rect.height) * 20);
+      setYOffsetAngle(((x - rect.width / 2) / rect.width) * 10 + 30);
+    }
   };
+
+  // useEffect(() => {
+  //   if (isFirstRender.current) {
+  //     isFirstRender.current = false;
+  //     return;
+  //   }
+  //   const timeout = setTimeout(() => {
+  //     if (focusState.current === 1) {
+  //       focus();
+  //     }
+  //   }, 1000);
+  //   return () => {
+  //     clearTimeout(timeout);
+  //   };
+  // }, [xOffsetAngle, yOffsetAngle]);
+
   const moveReset = () => {
+    focusState.current = 0;
     setTransitionTime(0.5);
     setXOffsetAngle(0);
     setYOffsetAngle(0);
-    clearTimeout(resetTimeout);
+    setScale(1);
   };
   const focus = () => {
+    focusState.current = 2;
     setTransitionTime(0.5);
-    setXOffsetAngle(0);
-    setYOffsetAngle(30);
+    // setXOffsetAngle(0);
+    setYOffsetAngle(yOffsetAngle / 2 + 30);
   };
 
   return (
@@ -44,9 +64,17 @@ export default function Portfolio() {
             <div
               className={styles.book}
               onMouseMove={moveTo}
+              onMouseEnter={() => {
+                focusState.current = 1;
+                setTimeout(() => {
+                  if (focusState.current === 1) {
+                    focus();
+                  }
+                }, 1000);
+              }}
               onMouseLeave={moveReset}
               style={{
-                transform: `rotateY(${yOffsetAngle}deg) rotateX(${xOffsetAngle}deg)`,
+                transform: `rotateX(${xOffsetAngle}deg) rotateY(${yOffsetAngle}deg) scale(${scale})`,
                 transition: `all ease ${transitionTime}s`,
               }}
             >
@@ -59,7 +87,7 @@ export default function Portfolio() {
             </div>
           </div>
           <h2 className={styles.description}>用 HTML & CSS 做的书形组件</h2>
-          <p>
+          <p style={{ textAlign: "center" }}>
             鼠标悬浮/手指点击 会旋转这本书
             <br />
             （虽然是用 React 写的👀）
